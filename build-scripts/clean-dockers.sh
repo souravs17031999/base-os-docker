34d73a3c3fc1dfd960800a2403cd39fc7bba3f21
souravs17031999 <souravs_1999@rediffmail.com>
2022-03-08 23:39:36 +0530

    wip

--- build-scripts/clean-dockers.sh
+++ build-scripts/clean-dockers.sh
@@ -3,7 +3,7 @@
 source $WORKSPACE/build-scripts/env.sh
 echo "Cleaning up dockers"
 
-docker-compose $BUILD_COMPOSE_LIST down --volumes --rmi local --remove-orphans
-docker-compose down --rmi all || true 
+# docker-compose $BUILD_COMPOSE_LIST down --volumes --rmi local --remove-orphans
+# docker-compose down --rmi all || true 
 
 echo "---------------------------------------------"
